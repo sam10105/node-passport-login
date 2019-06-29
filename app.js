0f60fde6265ef3eaee602f2a9d3be20ad90ab935
Sam Kasyoki <samkasyoki@gmail.com>
2019-06-29 17:09:58 +0300

    render homepage with ejs

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
 // Routes
 app.use("/", require("./routes/index"));
